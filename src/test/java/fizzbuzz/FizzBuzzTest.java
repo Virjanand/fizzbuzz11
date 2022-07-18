58b9fd05f03ea524bda3c8d8f4cadc921aa44d44
Virjanand Panday <virjanand@virjanand.com>
2022-07-18 11:23:41 +0200

    Red Test: replace 3 with Fizz

--- src/test/java/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/fizzbuzz/FizzBuzzTest.java
@@ -11,4 +11,10 @@ public class FizzBuzzTest {
         FizzBuzz fizzBuzz = new FizzBuzz();
         assertThat(fizzBuzz.fizzBuzzNumers()).hasSize(100);
     }
+
+    @Test
+    void replace3WithFizz() {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        assertThat(fizzBuzz.fizzBuzzNumers().get(2)).isEqualTo("Fizz");
+    }
 }
